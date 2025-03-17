228c390312e8785845aa03e9889d275792b874a6
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-03-17 22:12:20 +0530

    implement JWT authentication with refresh token storage and cookie management

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,37 @@
 import User from "../models/user.model.js";
+import { redis } from "../lib/redis.js";
+import jwt from "jsonwebtoken";
+
+const generateToken = (userId) => {
+  const accessToken = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "15m",
+  });
+
+  const refreshToken = jwt.sign({ userId }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: "7d",
+  });
+
+  return { accessToken, refreshToken };
+};
+
+const storeRefreshToken = async (userId, refreshToken) => {
+	await redis.set(`refresh_token:${userId}`, refreshToken, "EX", 7 * 24 * 60 * 60); // 7days
+};
+
+const setCookies = (res, accessToken, refreshToken) => {
+	res.cookie("accessToken", accessToken, {
+		httpOnly: true, // prevent XSS attacks, cross site scripting attack
+		secure: process.env.NODE_ENV === "production",
+		sameSite: "strict", // prevents CSRF attack, cross-site request forgery attack
+		maxAge: 15 * 60 * 1000, // 15 minutes
+	});
+	res.cookie("refreshToken", refreshToken, {
+		httpOnly: true, // prevent XSS attacks, cross site scripting attack
+		secure: process.env.NODE_ENV === "production",
+		sameSite: "strict", // prevents CSRF attack, cross-site request forgery attack
+		maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+	});
+};
 
 export const signup = async (req, res) => {
   const { email, password, name } = req.body;
@@ -11,6 +44,11 @@ export const signup = async (req, res) => {
     }
 
     const user = await User.create({ name, email, password });
+
+    const { accessToken, refreshToken } = generateToken(user._id);
+    await storeRefreshToken(user._id, refreshToken);
+
+    setCookies(res, accessToken, refreshToken);    
 
     return res.status(201).json({ user, message: "User created successfully" });
   } catch (error) {
